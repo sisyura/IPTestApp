ff639af7076d56cad143f37010b846647c401b5d
Nikita <nemov@deck.lc>
2024-08-29 16:48:44 +0300

    part

--- features/iptaskmain/src/main/java/ru/nemov/ittestmain/GetAllProductsUseCase.kt
+++ features/iptaskmain/src/main/java/ru/nemov/ittestmain/GetAllProductsUseCase.kt
@@ -1,8 +1,8 @@
 package ru.nemov.ittestmain
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import ru.nemov.ittask.data.ProductsRepository
-import ru.nemov.ittask.data.model.Product
 
 class GetAllProductsUseCase(
     private val repository: ProductsRepository
@@ -10,5 +10,12 @@ class GetAllProductsUseCase(
 
     operator fun invoke(): Flow<List<Product>> {
         return repository.getAllProducts()
+            .map { product ->
+                product.map { it.toUiProduct() }
+            }
     }
+}
+
+private fun ru.nemov.ittask.data.model.Product.toUiProduct(): Product {
+    TODO("Mot implemented")
 }
--- features/iptaskmain/src/main/java/ru/nemov/ittestmain/IPTaskViewModel.kt
+++ features/iptaskmain/src/main/java/ru/nemov/ittestmain/IPTaskViewModel.kt
@@ -1,9 +1,17 @@
 package ru.nemov.ittestmain
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import ru.nemov.ittask.data.ProductsRepository
 
-internal class IPTaskViewModel : ViewModel() {
+internal class IPTaskViewModel(
+    private val getAllProductsUseCase: GetAllProductsUseCase
+) : ViewModel() {
 
-//    val products: StateFlow<List<Product>>
+    val products: Flow<List<Product>> = getAllProductsUseCase.invoke()
+
 }
